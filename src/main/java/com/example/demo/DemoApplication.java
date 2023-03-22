b957d3ce96faaf90d07759994f8b847e79105f87
ArturasBidva <arturas.bidva@gmail.com>
2023-03-22 02:00:00 +0200

    build

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,9 @@ package com.example.demo;
 
 import com.example.demo.Models.Exercise;
 import com.example.demo.service.ExerciseService;
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
