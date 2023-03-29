d8786627b43052014ef6c351235e4c95d08d60e4
ArturasBidva <arturas.bidva@gmail.com>
2023-03-29 13:52:17 +0300

    update

--- src/main/java/com/example/demo/Models/User.java
+++ src/main/java/com/example/demo/Models/User.java
@@ -1,6 +1,8 @@
 package com.example.demo.Models;
 
 import com.example.demo.Entities.UserEntity;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
@@ -8,6 +10,17 @@ public class User {
     Long id;
     String username;
     String password;
+
+    public User() {} // default constructor
+
+    @JsonCreator
+    public User(@JsonProperty("id") Long id,
+                @JsonProperty("username") String username,
+                @JsonProperty("password") String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
 
     public User(UserEntity userEntity) {
         this.id = userEntity.getId();
